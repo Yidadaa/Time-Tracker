9cc2f2ada97b24806b28893c17d91e97d5d3ce82
Yidadaa <yidadaa@qq.com>
2019-01-14 17:57:35 +0800

    feat(home): 完成首页样式

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,6 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
@@ -27,15 +19,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -43,67 +26,180 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String title = "上午好";
+  final _theKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    BoxDecoration cardDecoration = BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.0),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black12, offset: Offset(0.0, 0.0), blurRadius: 10.0)
+        ]);
+
+    TextStyle boldStyle =
+        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold);
+    TextStyle boldTitleStyle =
+        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+    Expanded block = Expanded(
+        child: Container(
+      decoration: cardDecoration,
+      padding:
+          new EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0, bottom: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text("非必要活动"),
+              Container(
+                height: 10.0,
+                width: 10.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.green),
+              )
+            ],
+          ),
+          Text("睡觉", style: boldStyle),
+          Divider(
+            color: Colors.black12,
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text("08:03", style: boldStyle),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  child: Icon(Icons.pause),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    ));
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        key: _theKey,
+        bottomNavigationBar: BottomAppBar(
+          child: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home), title: Text("主页")),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.pie_chart), title: Text("汇总")),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), title: Text("设置"))
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        body: ListView(
+          padding: new EdgeInsets.all(20.0),
+          children: <Widget>[
+            Container(
+                margin: new EdgeInsets.only(top: 40.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                )),
+            Container(
+                margin: new EdgeInsets.only(top: 30.0, bottom: 40.0),
+                decoration: cardDecoration,
+                child: Material(
+                    child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Text("今日概览"),
+                                  ),
+                                  Text("2019/01/12")
+                                ],
+                              ),
+                              Container(
+                                height: 12.0,
+                                color: Colors.grey,
+                                margin: new EdgeInsets.only(
+                                    top: 20.0, bottom: 20.0),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text("已记录"),
+                                      Text(
+                                        "5h 40min",
+                                        style: boldStyle,
+                                      )
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text("最耗时"),
+                                      Text(
+                                        "睡觉、工作",
+                                        style: boldStyle,
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        )))),
+            Text(
+              "选择一项活动开始记录",
+              style: boldTitleStyle,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10.0),
+              child: Column(
+                  children: List.generate(4, (index) {
+                return Container(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      block,
+                      Container(
+                        width: 10.0,
+                      ),
+                      block
+                    ],
+                  ),
+                );
+              })),
+            )
+          ],
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
